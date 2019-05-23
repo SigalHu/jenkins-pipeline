31d1687175297503f03049375df74f4fdc3fa64d
xujun <xujun@gegejia.com>
2019-05-24 00:28:11 +0800

    use os.environ

--- py_pipeline/step1.py
+++ py_pipeline/step1.py
@@ -5,7 +5,7 @@
 """
 import os
 
-print(os.getenv("message1"))
-print(os.getenv("message2"))
-print(os.getenv("CONTEXT"))
-os.putenv("CONTEXT", os.getenv("CONTEXT") + "1")
+print(os.environ["message1"])
+print(os.environ["message2"])
+print(os.environ["CONTEXT"])
+os.environ["CONTEXT"] += "1"
