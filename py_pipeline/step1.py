cbba5521d557b36ef021a86d7d38d3e7db02d5a6
xujun <xujun@gegejia.com>
2019-05-23 23:13:48 +0800

    print CONTEXT

--- py_pipeline/step1.py
+++ py_pipeline/step1.py
@@ -8,6 +8,4 @@ import os
 print(os.getenv("message1"))
 print(os.getenv("message2"))
 print(os.getenv("CONTEXT"))
-os.putenv("message1", os.getenv("message1") + "1")
-os.putenv("message2", os.getenv("message1") + "1")
 os.putenv("CONTEXT", os.getenv("CONTEXT") + "1")
