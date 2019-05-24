8060031f78b760d5831ef85e16fc7a43e2e50363
xujun <xujun@gegejia.com>
2019-05-24 16:07:43 +0800

    print remote server dirlist

--- py_pipeline/step3.py
+++ py_pipeline/step3.py
@@ -8,12 +8,19 @@ import os
 import paramiko
 
 if __name__ == '__main__':
-    with paramiko.SSHClient() as ssh, \
-            paramiko.SFTPClient.from_transport(ssh.get_transport()) as sftp:
+    with paramiko.SSHClient() as ssh:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect("127.0.0.1", username=os.getenv("ACCESS_KEY_USR"), key_filename=os.getenv("ACCESS_KEY"))
-        stdin, stdout, stderr = ssh.exec_command("ls")
-        print(stdout.read())
 
-        attr = sftp.put("py_pipeline/step2.py", "py_pipeline_step2.py")
-        print(attr)
+        sftp = paramiko.SFTPClient.from_transport(ssh.get_transport())
+        sftp.put("py_pipeline/step2.py", "py_pipeline_step2.py")
+        print("SFTP listdir start...")
+        for dir_str in sftp.listdir():
+            print(dir_str)
+        print("SFTP listdir end.")
+
+        stdin, stdout, stderr = ssh.exec_command("ls")
+        print("SSH ls start..")
+        for line in stdout.readlines():
+            print(str(line).replace("\n", ""))
+        print("SSH ls end.")
