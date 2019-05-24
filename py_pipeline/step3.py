57bb5e98f3e75eb308d16b5ed196e9a3da17e783
xujun <xujun@gegejia.com>
2019-05-24 15:45:20 +0800

    防止资源泄露

--- py_pipeline/step3.py
+++ py_pipeline/step3.py
@@ -8,12 +8,12 @@ import os
 import paramiko
 
 if __name__ == '__main__':
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('127.0.0.1', username=os.getenv('ACCESS_KEY_USR'), key_filename=os.getenv('ACCESS_KEY'))
-    stdin, stdout, stderr = ssh.exec_command('ls')
-    print(stdout.read())
-    sftp = paramiko.SFTPClient.from_transport(ssh.get_transport())
-    attr = sftp.put("py_pipeline/step2.py", "py_pipeline_step2.py")
-    sftp.close()
-    ssh.close()
+    with paramiko.SSHClient() as ssh, \
+            paramiko.SFTPClient.from_transport(ssh.get_transport()) as sftp:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect("127.0.0.1", username=os.getenv("ACCESS_KEY_USR"), key_filename=os.getenv("ACCESS_KEY"))
+        stdin, stdout, stderr = ssh.exec_command("ls")
+        print(stdout.read())
+
+        attr = sftp.put("py_pipeline/step2.py", "py_pipeline_step2.py")
+        print(attr)
