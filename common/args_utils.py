e4b9318d5e274b41fdb27c5c46d8acdf908be7b2
xujun <xujun@gegejia.com>
2019-06-13 11:52:27 +0800

    忽略第一个命令行参数

--- common/args_utils.py
+++ common/args_utils.py
@@ -9,7 +9,7 @@ def parse_args_to_dict(args: list, default_dict=None) -> dict:
     if default_dict is None:
         default_dict = {}
     args_dict = {}
-    for ii in range(1, len(args), 2):
+    for ii in range(2, len(args), 2):
         args_dict[args[ii - 1]] = args[ii]
     for k, v in default_dict.items():
         if k not in args_dict.keys():
