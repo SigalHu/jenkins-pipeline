d33f45fdaf27765d11d0766e9ebefd2f89136a33
xujun <xujun@gegejia.com>
2019-07-07 01:06:25 +0800

    保存用户配置

--- scripts/save_user_config.py
+++ scripts/save_user_config.py
@@ -15,13 +15,16 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] [%(name)s] %(message)s")
     jenkins_home = os.getenv("JENKINS_HOME")
     job_name = os.getenv("JOB_NAME")
-    assert_utils.all_not_empty(jenkins_home=jenkins_home, job_name=job_name)
+    job_url = os.getenv("JOB_URL")
+    assert_utils.all_not_empty(jenkins_home=jenkins_home, job_name=job_name, job_url=job_url)
+
     config_file = os.path.join(jenkins_home, "jobs", job_name, "config.xml")
     config_content = io_utils.load_file(config_file)
     print("message3=" + os.getenv("message3"))
-    config_content.replace("test123", os.getenv("message3"))
+    config_content = config_content.replace("test123", os.getenv("message3"))
     print("content=" + config_content)
     io_utils.save_file(config_file, config_content)
-    resp = requests.post("http://{}:{}@localhost:8080/job/{}/config.xml".format(sys.argv[1], sys.argv[2], job_name),
-                         data="@" + config_file, headers={"Content-Type": "text/xml"})
+
+    resp = requests.post(job_url + "config.xml", auth=(sys.argv[1], sys.argv[2]),
+                         data=open(config_file, "rb"), headers={"Content-Type": "text/xml"})
     print("resp=" + resp.text)
