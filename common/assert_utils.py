b8e18ccb7c293d50668acab14a59a31d1f5d7e75
xujun <xujun@gegejia.com>
2019-07-07 00:34:56 +0800

    update utils

--- common/assert_utils.py
+++ common/assert_utils.py
@@ -3,19 +3,6 @@
 @author huxujun
 @date 2019-06-14
 """
-from common import net_utils
-
-
-def valid_port(host: str, port: int):
-    assert port, "The port {}:{} is invalid!".format(host, port)
-    is_connect, ex = net_utils.ping(host, port)
-    assert not is_connect, "The port {}:{} already be in use!".format(host, port)
-
-
-def port_in_use(host: str, port: int):
-    assert port, "The port {}:{} is invalid!".format(host, port)
-    is_connect, ex = net_utils.ping(host, port)
-    assert is_connect, "The port {}:{} must be in use!".format(host, port)
 
 
 def all_not_empty(**kwargs):
--- common/io_utils.py
+++ common/io_utils.py
@@ -3,13 +3,8 @@
 @author huxujun
 @date 2019-06-17
 """
-import configparser
-import html
 import logging
 import os
-import shutil
-import tempfile
-from urllib import parse
 
 
 def load_file(filename):
@@ -29,45 +24,6 @@ def save_file(filename, content=""):
             f.write(content)
     except:
         logging.exception("Error to write file %s", filename)
-
-
-def copy_file(src_file, dest_file):
-    try:
-        shutil.copyfile(src_file, dest_file)
-        return True
-    except:
-        logging.exception("Error to copy src_file %s to dest_file %s", src_file, dest_file)
-        return False
-
-
-def unescape_file(filename):
-    template_name = "{}.tmp".format(filename)
-    try:
-        with open(filename) as f, open(template_name, "w") as t:
-            for line in f:
-                line = html.unescape(line)
-                line = parse.unquote(line)
-                t.write(line)
-        os.rename(template_name, filename)
-    except:
-        logging.exception("Error to unescape file %s", filename)
-        os.remove(template_name)
-
-
-def load_properties_file(filename):
-    cf = configparser.ConfigParser()
-    with open(filename) as d, tempfile.NamedTemporaryFile("w+") as t:
-        t.write("[default]\n{}".format(d.read()))
-        t.flush()
-        cf.read(t.name)
-        return dict(cf.items("default"))
-
-
-def load_cfg_file(filename):
-    assert os.path.isfile(filename), "The file {} must be exist!".format(filename)
-    cf = configparser.ConfigParser()
-    cf.read(filename)
-    return cf
 
 
 if __name__ == '__main__':
